7a15587d84619866f2c82dd387550d6c7bfa54f1
vk-429 <vkumar76429@gmail.com>
2024-05-25 17:08:11 +0530

    Learnt Comments and Escape Sequences in Python

---
+++ Basics/CommentsandEscapeSequences.py
@@ -0,0 +1,27 @@
+# Hey Vivek, Please don't remove this line
+# Author: Harry
+# Course: 100DaysofCode
+print("Hey I am a \"good boy\"\nand this viewer is also a good boy/girl")
+
+# This is 'single line comment'
+print("This is print statement.")
+print("Hello World !!!")#Printing Hello World
+
+# /" print "
+# /' print '
+
+'''Multiline Comment
+    2nd line
+    3rd line
+'''
+"""This
+is
+also
+valid as a multiline 
+comment"""
+
+# Or just select all lines and ctrl+/ to comment all the lines
+
+# Default separator ' '
+print("Hey",6,7,sep="~",end="009\n")
+print("Vivek")
